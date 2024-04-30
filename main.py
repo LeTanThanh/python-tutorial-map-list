7b08c4e7fd32a547c304882c565fae47c954fd33
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-30 22:25:05 +0700

    Introduction to the Python map() function

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 if __name__ == "__main__":
-  print("How to Transform List Elements with Python map() Function")
+  # Introduction to the Python map() function
+
+  bonuses = [100, 200, 300]
+  print(bonuses)
+
+  new_bonuses = []
+  for bunus in bonuses:
+    new_bonuses.append(bunus * 2)
+  print(new_bonuses)
+
+  """
+  iterator = map(function, list)
+  """
+
+  bonuses = [100, 200, 300]
+  print(bonuses)
+
+  def double(bonus):
+    return bonus * 2
+
+  iterator = map(double, bonuses)
+  print(list(iterator))
+
+  iterator = map(lambda bonus: bonus * 2, bonuses)
+  print(list(iterator))
